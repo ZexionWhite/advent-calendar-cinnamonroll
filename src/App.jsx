290f1a9439bce8e59080f230ff00dfb056a94407
Facundo White <drsaucedofacundo@gmail.com>
2025-11-30 19:40:15 -0300

    fix:package.json fixed

--- src/App.jsx
+++ src/App.jsx
@@ -55,11 +55,11 @@ function getTodayInfo() {
 function isDayUnlocked(day) {
   const { month, date } = getTodayInfo();
 
-  // if (month < 11) return false;
+  if (month < 11) return false;
 
-  // if (month === 11) {
-  //   return date >= day;
-  // }
+  if (month === 11) {
+    return date >= day;
+  }
 
   return true;
 }
